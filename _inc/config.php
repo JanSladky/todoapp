1bbdfc6d3f242a63a4fd373985db2ad8b97d5155
Jan Sladký <sladky.honza@gmail.com>
2020-04-30 00:20:14 +0200

    todoapp

---
+++ _inc/config.php
@@ -0,0 +1,26 @@
+<?php
+
+// show all errors
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+
+
+// require stuff
+require_once 'vendor/autoload.php';
+
+//global variable
+$base_url = 'http://localhost/todoapp';
+
+// Using Medoo namespace
+use Medoo\Medoo;
+
+// connect to db
+$database = new Medoo([
+	'database_type' => 'mysql',
+	'database_name' => 'todo',
+	'server'        => 'localhost',
+	'username'      => 'root',
+	'password'      => '',
+	'charset'       => 'utf8'
+]);
